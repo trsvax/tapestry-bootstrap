af5c67429a9573e071be029568fdca96c99b6482
Barry Books <bfb@trsvax.com>
2011-11-10 06:46:39 -0600

    Add cycle widget

--- src/main/java/com/trsvax/bootstrap/mixins/JQLiteCycle.java
+++ src/main/java/com/trsvax/bootstrap/mixins/JQLiteCycle.java
@@ -3,7 +3,7 @@ package com.trsvax.bootstrap.mixins;
 import org.apache.tapestry5.annotations.Import;
 import org.got5.tapestry5.jquery.mixins.ui.Widget;
 
-@Import(library="classpath:/com/trsvax/bootstrap/misc/jquery-infinitescroll.js")
-public class InfiniteScroll extends Widget {
+@Import(library="classpath:/com/trsvax/bootstrap/misc/jqlite-cycle-uber-lite-min.js")
+public class JQLiteCycle extends Widget {
 
 }

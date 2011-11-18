918aa08c7c0b73ab9151924bf10a303b3fb0fbc7
Barry Books <bfb@trsvax.com>
2011-11-18 12:50:49 -0600

    add select form type

--- src/test/java/com/trsvax/bootstrap/pages/beanedit/BeanEditIndex.java
+++ src/test/java/com/trsvax/bootstrap/pages/beanedit/BeanEditIndex.java
@@ -1,12 +1,17 @@
 package com.trsvax.bootstrap.pages.beanedit;
 
 import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.PageActivationContext;
 import org.apache.tapestry5.annotations.Property;
 
 import com.trsvax.bootstrap.Address;
 
 @Import(library="classpath:/com/trsvax/bootstrap/validation.js")
 public class BeanEditIndex {
+	
+	@PageActivationContext
+	@Property
+	private String formtype;
 
 	@Property
 	private Address address;

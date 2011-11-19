ea97b8fc8e75cda6ebf4b5e5cc61cd74abe9fb90
Barry Books <bfb@trsvax.com>
2011-11-19 09:21:02 -0600

    BeanEditor cleanup

--- src/main/java/com/trsvax/bootstrap/components/BeanEditForm.java
+++ src/main/java/com/trsvax/bootstrap/components/BeanEditForm.java
@@ -3,19 +3,16 @@ package com.trsvax.bootstrap.components;
 import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.SupportsInformalParameters;
-import org.apache.tapestry5.corelib.components.BeanEditForm;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 
 @SupportsInformalParameters
 @Import(library="classpath:/com/trsvax/bootstrap/validation.js")
-public class BootStrapBeanEditForm extends BeanEditForm {
+public class BeanEditForm extends org.apache.tapestry5.corelib.components.BeanEditForm {
 	
 	@Inject
 	private ComponentResources resources;
-	
-
-	
+		
 	public String getFormType() {
 		String formtype = resources.getInformalParameter("class", String.class);
 		if ( formtype != null ) {
--- src/main/java/com/trsvax/bootstrap/components/BeanEditor.java
+++ src/main/java/com/trsvax/bootstrap/components/BeanEditor.java
@@ -1,25 +1,13 @@
 package com.trsvax.bootstrap.components;
 
 import org.apache.tapestry5.ComponentResources;
-import org.apache.tapestry5.Field;
-import org.apache.tapestry5.ValidationTracker;
-import org.apache.tapestry5.annotations.Environmental;
-import org.apache.tapestry5.corelib.components.BeanEditor;
 import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.services.FormSupport;
 
-public class BootStrapBeanEditor extends BeanEditor {
+public class BeanEditor extends org.apache.tapestry5.corelib.components.BeanEditor {
 	
 	@Inject
 	private ComponentResources resources;
-	
-	@Environmental
-	private ValidationTracker validationTracker;
-	
-	@Environmental
-	private FormSupport formSupport;
-	
 	
 	
 	public String getLegend() {

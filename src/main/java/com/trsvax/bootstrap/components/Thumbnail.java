684c5862d90b31ea1afa8e6f6f4b60e743a5bb69
Barry Books <bfb@trsvax.com>
2012-02-12 13:18:07 -0600

    add informal parameters

--- src/main/java/com/trsvax/bootstrap/components/Thumbnail.java
+++ src/main/java/com/trsvax/bootstrap/components/Thumbnail.java
@@ -1,5 +1,19 @@
 package com.trsvax.bootstrap.components;
 
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.MarkupWriter;
+import org.apache.tapestry5.annotations.BeginRender;
+import org.apache.tapestry5.annotations.SupportsInformalParameters;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+@SupportsInformalParameters
 public class Thumbnail {
 
+	@Inject
+	private ComponentResources resources;
+	
+	@BeginRender
+	void beginRender(MarkupWriter writer) {
+		resources.renderInformalParameters(writer);
+	}
 }

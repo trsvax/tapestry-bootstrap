c19ece2b6533fcd56731a605e73f08d71fa4a154
Barry Books <bfb@trsvax.com>
2012-02-19 14:09:39 -0600

    cleanup

--- src/main/resources/com/trsvax/bootstrap/assets/mixins/draggable/draggable.js
+++ src/main/resources/com/trsvax/bootstrap/assets/mixins/draggable/draggable.js
@@ -2,7 +2,7 @@
     /** Container of functions that may be invoked by the Tapestry.init() function. */
     $.extend(Tapestry.Initializer, {
     	jqDraggable: function(specs){
-    		$( specs.selector ).draggable(specs.params);
+    		$( specs.selector ).draggable(specs.params);   		
         }
     });
 })(jQuery);

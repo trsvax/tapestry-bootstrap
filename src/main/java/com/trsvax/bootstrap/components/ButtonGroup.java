a7cb5df542f03b38b58709542cca8d1b14661e55
Barry Books <bfb@trsvax.com>
2012-03-30 17:35:04 -0500

    Some javascript mixins

--- src/main/java/com/trsvax/bootstrap/components/ButtonGroup.java
+++ src/main/java/com/trsvax/bootstrap/components/ButtonGroup.java
@@ -1,21 +1,41 @@
 package com.trsvax.bootstrap.components;
 
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.MarkupWriter;
+import org.apache.tapestry5.annotations.AfterRender;
+import org.apache.tapestry5.annotations.BeginRender;
 import org.apache.tapestry5.annotations.CleanupRender;
 import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.annotations.SupportsInformalParameters;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Environment;
 
 import com.trsvax.bootstrap.environment.ButtonGroupEnvironment;
 import com.trsvax.bootstrap.environment.ButtonGroupValues;
 
+@SupportsInformalParameters
 public class ButtonGroup {
 	
  	@Inject
   	private Environment environment;
+ 	
+ 	@Inject
+ 	private ComponentResources resources;
 	
 	@SetupRender
 	void setupRender() {
 		environment.push(ButtonGroupEnvironment.class, new ButtonGroupValues(null).withButtonGroup(true));
+	}
+	
+	@BeginRender
+	void beginRender(MarkupWriter writer) {
+		writer.element("div", "class","btn-group");
+		resources.renderInformalParameters(writer);
+	}
+	
+	@AfterRender
+	void afterRender(MarkupWriter writer) {
+		writer.end();
 	}
 	
 	@CleanupRender
---
+++ src/main/java/com/trsvax/bootstrap/mixins/Popover.java
@@ -0,0 +1,28 @@
+package com.trsvax.bootstrap.mixins;
+
+import org.apache.tapestry5.ClientElement;
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.annotations.BeginRender;
+import org.apache.tapestry5.annotations.MixinAfter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
+
+
+@MixinAfter
+public class Popover {
+	
+	@Inject
+	private ComponentResources resources;
+	
+	@Inject
+	private JavaScriptSupport javaScriptSupport;
+	
+	@BeginRender
+	void beginRender() {
+		//just blow up if not a clientElement
+		String id = ((ClientElement) resources.getContainer()).getClientId();
+		//TODO fix me
+		javaScriptSupport.addScript("$('#%s').popover()", id);
+	}
+
+}

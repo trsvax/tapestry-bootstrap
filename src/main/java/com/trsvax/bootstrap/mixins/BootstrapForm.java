d8c732af21c3e2727e3e19f8f3d4b0a087d9388f
Barry Books <bfb@trsvax.com>
2013-06-06 09:53:30 -0500

    Cleanup and Refactor

--- src/main/java/com/trsvax/bootstrap/mixins/BootstrapForm.java
+++ src/main/java/com/trsvax/bootstrap/mixins/BootstrapForm.java
@@ -17,57 +17,89 @@ import org.slf4j.Logger;
 public class BootstrapForm {
 	
 	private Element form;
+	private List<Element> removeElements;
+	private boolean broken;
+	private String formClass;
 	
 	@Inject
 	private Logger logger;
-	
+		
 	@BeginRender
 	void beginRender(MarkupWriter writer) {
-		form = writer.getElement();
+		form = writer.getElement();	
 	}
 	
-	private List<Element> divs;
-	private boolean broken;
 	
 	@CleanupRender
 	void cleanupRender() {
-		String formClass = form.getAttribute("class");
-		if ( formClass != null && formClass.contains("form-horizontal")) {
+		formClass = form.getAttribute("class");
+		if ( isHorizontalForm()) {
 			return;
 		}
-		divs = new ArrayList<Element>();
+		removeElements = new ArrayList<Element>();
 		form.visit( new Visitor() {
 			
 			public void visit(Element element) {
 				String className = element.getAttribute("class");
-				if ( element.getName().equals("div") && (isControl(className) || isFormActions(className)) ) {
-					divs.add(element);
+				if ( isDiv(element) && (isControlClass(className) || isFormActions(className)) ) {
+					removeElements.add(element);
 				}			
-				if ( className != null && className.contains("control-label")) {
-					String labelClass = className.replace("control-label", "");
-					if ( labelClass.length() == 0 ) {
-						element.forceAttributes("class",null);
-					} else {
-						element.forceAttributes("class",labelClass);
-					}
+				if ( isControLabel(className)) {
+					String labelClass = className.replace("control-label", "").trim();
+					element.forceAttributes("class", labelClass.length() == 0 ? null : labelClass);
 				}
-				if ( element.getName().equals("div") && isButtonGroup(className) && ! broken) {
-					element.elementBefore("br");
-					broken = true;
+				if ( isDiv(element) && isButtonGroup(className) && needBR() ) {
+						element.elementBefore("br");
 				}
-				
-				
+				if ( isButtonGroup(className) && removeButtonGroup()) {
+					removeElements.add(element);
+				}				
 			}
 		});
-		for ( Element div : divs ) {
-			div.pop();
+		for ( Element element : removeElements ) {
+			element.pop();
 		}
-		List<Node> children = form.getChildren();
-		Element fieldset = form.element("fieldset");
-		for ( Node child : children ) {
-			child.moveToBottom(fieldset);
+		if ( needFieldSet()) {
+			List<Node> children = form.getChildren();
+			Element fieldset = form.element("fieldset");
+			for ( Node child : children ) {
+				child.moveToBottom(fieldset);
+			}
 		}
 		
+	}
+	
+	private boolean isDiv(Element element) {
+		return "div".equals( element.getName());
+	}
+	
+	private boolean isControLabel(String className) {
+		if ( className == null ) {
+			return false;
+		}
+		return className.contains("control-label");
+	}
+	
+	private boolean needBR() {
+		if ( broken || isInlineForm() || isSearchForm() ) {
+			return false;
+		}
+		broken = true;
+		return true;
+	}
+	
+	private boolean removeButtonGroup() {
+		if ( isInlineForm() || isSearchForm() ) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean needFieldSet() {
+		if ( isDefaultForm() ) {
+			return true;
+		}
+		return false;
 	}
 	
 	private boolean isButtonGroup(String className) {
@@ -90,7 +122,7 @@ public class BootstrapForm {
 		return false;
 	}
 	
-	private boolean isControl(String className) {
+	private boolean isControlClass(String className) {
 		if ( className == null ) {
 			return false;
 		}
@@ -98,6 +130,35 @@ public class BootstrapForm {
 			return true;
 		}
 		if ( className.contains("controls")) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isDefaultForm() {
+		if ( isSearchForm() || isInlineForm() || isHorizontalForm() ) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private boolean isSearchForm() {
+		if ( "form-search".equals(formClass)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isInlineForm() {
+		if ( "form-inline".equals(formClass)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isHorizontalForm() {
+		if ( "form-horizontal".equals(formClass)) {
 			return true;
 		}
 		return false;
